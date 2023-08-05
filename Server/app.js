164d86d668fafacbc304ea9b9bb6d06f1ef1a657
Abhishek Sengupta <chotuame@gmail.com>
2023-08-05 22:03:50 +0530

    deploying.

--- Server/app.js
+++ Server/app.js
@@ -9,18 +9,12 @@ require('./db/conn');
 
 const User = require('./model/userSchema');
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(200);
-    }
-    next();
-  });
+const corsConfig = {
+    credentials: true,
+    origin: true,
+};
+app.use(cors(corsConfig));
+
 
 app.use(express.json());
 
